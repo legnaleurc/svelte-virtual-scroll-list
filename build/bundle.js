7f773c5798ee8cc328899109d87202e29191365c
v1ack <kirilkin12@gmail.com>
2021-07-18 10:51:37 +0000

    Deploying to gh-pages from  @ 2cd16376a1ef81dddb2412e8df6c41eb407b011f 🚀

--- build/bundle.js
+++ build/bundle.js
@@ -117,11 +117,119 @@ function loop(callback) {
     };
 }
 
+// Track which nodes are claimed during hydration. Unclaimed nodes can then be removed from the DOM
+// at the end of hydration without touching the remaining nodes.
+let is_hydrating = false;
+function start_hydrating() {
+    is_hydrating = true;
+}
+function end_hydrating() {
+    is_hydrating = false;
+}
+function upper_bound(low, high, key, value) {
+    // Return first index of value larger than input value in the range [low, high)
+    while (low < high) {
+        const mid = low + ((high - low) >> 1);
+        if (key(mid) <= value) {
+            low = mid + 1;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return low;
+}
+function init_hydrate(target) {
+    if (target.hydrate_init)
+        return;
+    target.hydrate_init = true;
+    // We know that all children have claim_order values since the unclaimed have been detached
+    const children = target.childNodes;
+    /*
+    * Reorder claimed children optimally.
+    * We can reorder claimed children optimally by finding the longest subsequence of
+    * nodes that are already claimed in order and only moving the rest. The longest
+    * subsequence subsequence of nodes that are claimed in order can be found by
+    * computing the longest increasing subsequence of .claim_order values.
+    *
+    * This algorithm is optimal in generating the least amount of reorder operations
+    * possible.
+    *
+    * Proof:
+    * We know that, given a set of reordering operations, the nodes that do not move
+    * always form an increasing subsequence, since they do not move among each other
+    * meaning that they must be already ordered among each other. Thus, the maximal
+    * set of nodes that do not move form a longest increasing subsequence.
+    */
+    // Compute longest increasing subsequence
+    // m: subsequence length j => index k of smallest value that ends an increasing subsequence of length j
+    const m = new Int32Array(children.length + 1);
+    // Predecessor indices + 1
+    const p = new Int32Array(children.length);
+    m[0] = -1;
+    let longest = 0;
+    for (let i = 0; i < children.length; i++) {
+        const current = children[i].claim_order;
+        // Find the largest subsequence length such that it ends in a value less than our current value
+        // upper_bound returns first greater value, so we subtract one
+        const seqLen = upper_bound(1, longest + 1, idx => children[m[idx]].claim_order, current) - 1;
+        p[i] = m[seqLen] + 1;
+        const newLen = seqLen + 1;
+        // We can guarantee that current is the smallest value. Otherwise, we would have generated a longer sequence.
+        m[newLen] = i;
+        longest = Math.max(newLen, longest);
+    }
+    // The longest increasing subsequence of nodes (initially reversed)
+    const lis = [];
+    // The rest of the nodes, nodes that will be moved
+    const toMove = [];
+    let last = children.length - 1;
+    for (let cur = m[longest] + 1; cur != 0; cur = p[cur - 1]) {
+        lis.push(children[cur - 1]);
+        for (; last >= cur; last--) {
+            toMove.push(children[last]);
+        }
+        last--;
+    }
+    for (; last >= 0; last--) {
+        toMove.push(children[last]);
+    }
+    lis.reverse();
+    // We sort the nodes being moved to guarantee that their insertion order matches the claim order
+    toMove.sort((a, b) => a.claim_order - b.claim_order);
+    // Finally, we move the nodes
+    for (let i = 0, j = 0; i < toMove.length; i++) {
+        while (j < lis.length && toMove[i].claim_order >= lis[j].claim_order) {
+            j++;
+        }
+        const anchor = j < lis.length ? lis[j] : null;
+        target.insertBefore(toMove[i], anchor);
+    }
+}
 function append(target, node) {
-    target.appendChild(node);
+    if (is_hydrating) {
+        init_hydrate(target);
+        if ((target.actual_end_child === undefined) || ((target.actual_end_child !== null) && (target.actual_end_child.parentElement !== target))) {
+            target.actual_end_child = target.firstChild;
+        }
+        if (node !== target.actual_end_child) {
+            target.insertBefore(node, target.actual_end_child);
+        }
+        else {
+            target.actual_end_child = node.nextSibling;
+        }
+    }
+    else if (node.parentNode !== target) {
+        target.appendChild(node);
+    }
 }
 function insert(target, node, anchor) {
-    target.insertBefore(node, anchor || null);
+    if (is_hydrating && !anchor) {
+        append(target, node);
+    }
+    else if (node.parentNode !== target || (anchor && node.nextSibling !== anchor)) {
+        target.insertBefore(node, anchor || null);
+    }
 }
 function detach(node) {
     node.parentNode.removeChild(node);
@@ -652,6 +760,7 @@ function init(component, options, instance, create_fragment, not_equal, props, d
     $$.fragment = create_fragment ? create_fragment($$.ctx) : false;
     if (options.target) {
         if (options.hydrate) {
+            start_hydrating();
             const nodes = children(options.target);
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             $$.fragment && $$.fragment.l(nodes);
@@ -664,6 +773,7 @@ function init(component, options, instance, create_fragment, not_equal, props, d
         if (options.intro)
             transition_in(component.$$.fragment);
         mount_component(component, options.target, options.anchor, options.customElement);
+        end_hydrating();
         flush();
     }
     set_current_component(parent_component);
@@ -1041,7 +1151,7 @@ class Virtual {
     }
 }
 
-/* src/Item.svelte generated by Svelte v3.38.2 */
+/* src/Item.svelte generated by Svelte v3.38.3 */
 
 function create_fragment$8(ctx) {
 	let div;
@@ -1068,7 +1178,7 @@ function create_fragment$8(ctx) {
 		p(ctx, [dirty]) {
 			if (default_slot) {
 				if (default_slot.p && (!current || dirty & /*$$scope*/ 16)) {
-					update_slot(default_slot, default_slot_template, ctx, /*$$scope*/ ctx[4], dirty, null, null);
+					update_slot(default_slot, default_slot_template, ctx, /*$$scope*/ ctx[4], !current ? -1 : dirty, null, null);
 				}
 			}
 		},
@@ -1147,7 +1257,7 @@ class Item extends SvelteComponent {
 	}
 }
 
-/* src/VirtualScroll.svelte generated by Svelte v3.38.2 */
+/* src/VirtualScroll.svelte generated by Svelte v3.38.3 */
 const get_footer_slot_changes = dirty => ({ data: dirty[0] & /*displayItems*/ 8 });
 const get_footer_slot_context = ctx => ({ data: /*data*/ ctx[2] });
 
@@ -1230,7 +1340,7 @@ function create_default_slot_2(ctx) {
 		p(ctx, dirty) {
 			if (header_slot) {
 				if (header_slot.p && (!current || dirty[0] & /*$$scope, displayItems*/ 536870920)) {
-					update_slot(header_slot, header_slot_template, ctx, /*$$scope*/ ctx[29], dirty, get_header_slot_changes, get_header_slot_context);
+					update_slot(header_slot, header_slot_template, ctx, /*$$scope*/ ctx[29], !current ? [-1, -1] : dirty, get_header_slot_changes, get_header_slot_context);
 				}
 			}
 		},
@@ -1272,7 +1382,7 @@ function create_default_slot_1(ctx) {
 		p(ctx, dirty) {
 			if (default_slot) {
 				if (default_slot.p && (!current || dirty[0] & /*$$scope, displayItems*/ 536870920)) {
-					update_slot(default_slot, default_slot_template, ctx, /*$$scope*/ ctx[29], dirty, get_default_slot_changes, get_default_slot_context);
+					update_slot(default_slot, default_slot_template, ctx, /*$$scope*/ ctx[29], !current ? [-1, -1] : dirty, get_default_slot_changes, get_default_slot_context);
 				}
 			}
 		},
@@ -1419,7 +1529,7 @@ function create_default_slot$5(ctx) {
 		p(ctx, dirty) {
 			if (footer_slot) {
 				if (footer_slot.p && (!current || dirty[0] & /*$$scope, displayItems*/ 536870920)) {
-					update_slot(footer_slot, footer_slot_template, ctx, /*$$scope*/ ctx[29], dirty, get_footer_slot_changes, get_footer_slot_context);
+					update_slot(footer_slot, footer_slot_template, ctx, /*$$scope*/ ctx[29], !current ? [-1, -1] : dirty, get_footer_slot_changes, get_footer_slot_context);
 				}
 			}
 		},
@@ -1996,7 +2106,7 @@ function asyncTimeout(time) {
     return new Promise(resolve => setTimeout(resolve, time))
 }
 
-/* example/TestItem.svelte generated by Svelte v3.38.2 */
+/* example/TestItem.svelte generated by Svelte v3.38.3 */
 
 function create_fragment$6(ctx) {
 	let div;
@@ -2057,7 +2167,7 @@ class TestItem extends SvelteComponent {
 	}
 }
 
-/* example/SimpleList.svelte generated by Svelte v3.38.2 */
+/* example/SimpleList.svelte generated by Svelte v3.38.3 */
 
 function get_each_context$2(ctx, list, i) {
 	const child_ctx = ctx.slice();
@@ -2450,7 +2560,7 @@ function writable(value, start = noop) {
     return { set, update, subscribe };
 }
 
-/* example/SimpleListStore.svelte generated by Svelte v3.38.2 */
+/* example/SimpleListStore.svelte generated by Svelte v3.38.3 */
 
 function get_each_context$1(ctx, list, i) {
 	const child_ctx = ctx.slice();
@@ -2822,7 +2932,7 @@ class SimpleListStore extends SvelteComponent {
 	}
 }
 
-/* example/InfiniteList.svelte generated by Svelte v3.38.2 */
+/* example/InfiniteList.svelte generated by Svelte v3.38.3 */
 
 function create_default_slot$2(ctx) {
 	let testitem;
@@ -3086,7 +3196,7 @@ class InfiniteList extends SvelteComponent {
 	}
 }
 
-/* example/PageList.svelte generated by Svelte v3.38.2 */
+/* example/PageList.svelte generated by Svelte v3.38.3 */
 
 function create_default_slot$1(ctx) {
 	let testitem;
@@ -3310,7 +3420,7 @@ class PageList extends SvelteComponent {
 	}
 }
 
-/* example/ChangeableData.svelte generated by Svelte v3.38.2 */
+/* example/ChangeableData.svelte generated by Svelte v3.38.3 */
 
 function create_default_slot(ctx) {
 	let testitem;
@@ -3577,7 +3687,7 @@ class ChangeableData extends SvelteComponent {
 	}
 }
 
-/* example/App.svelte generated by Svelte v3.38.2 */
+/* example/App.svelte generated by Svelte v3.38.3 */
 
 function get_each_context(ctx, list, i) {
 	const child_ctx = ctx.slice();
